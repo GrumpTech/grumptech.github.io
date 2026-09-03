60a64214285556b34e3f34d19a4e12c2b711300b
GrumpTech <145297081+GrumpTech@users.noreply.github.com>
2026-09-03 08:07:37 +0200

    Update product page

--- products/formly-designer/chunk-DCGYWC_9.js
+++
@@ -1,2 +0,0 @@
-import {p as py,m as mv}from'./main-TWT3GNWL.js';var r=(()=>{class e{static \u0275fac=function(t){return new(t||e)};static \u0275cmp=py({type:e,selectors:[["thank-you"]],decls:1,vars:0,template:function(t,a){t&1&&mv(0,`Thank you!
-`);},styles:["[_nghost-%COMP%]{display:flex;flex-direction:column;align-items:center;justify-content:center;font-size:20px;height:100%}"]})}return e})();export{r as ThankYouPage};
